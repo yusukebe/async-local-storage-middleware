365d1ce906c8961ba336e5ca8269da30a86ca6f0
Yusuke Wada <yusuke@kamawada.com>
2024-01-25 21:22:30 +0900

    support env types

--- src/async-local-storage.ts
+++ src/async-local-storage.ts
@@ -1,18 +1,16 @@
 import { AsyncLocalStorage } from 'node:async_hooks'
 import { createMiddleware } from 'hono/factory'
-import { Context } from 'hono'
+import { Context, Env } from 'hono'
 
-type Callback<T> = (c: Context) => T
+type Callback<T, E extends Env> = (c: Context<E>) => T
 
 export const asyncLocalStorage = <T>() => {
   const localStorage = new AsyncLocalStorage<T>()
   return {
-    store: (callback: Callback<T>) =>
+    store: <E extends Env = Env>(callback: Callback<T, E>) =>
       createMiddleware(async (c, next) => {
         const obj = callback(c)
-        return localStorage.run(obj, () => {
-          next()
-        })
+        return localStorage.run(obj, next)
       }),
     get: (key: keyof T) => {
       const store = localStorage.getStore() as T
