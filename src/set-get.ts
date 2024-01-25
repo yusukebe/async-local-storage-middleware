dc140421d7a80af96a4817be27a621c90988dcea
Yusuke Wada <yusuke@kamawada.com>
2024-01-25 22:18:36 +0900

    add `set-get`

---
+++ src/set-get.ts
@@ -0,0 +1,16 @@
+import { Hono } from 'hono'
+import type { Log } from './types'
+
+const app = new Hono<{ Variables: Log }>()
+
+app.use(async (c, next) => {
+  c.set('requestId', crypto.randomUUID())
+  c.set('city', (c.req.raw.cf?.city ?? 'nowhere') as string)
+  await next()
+})
+
+app.get('/', async (c) => {
+  return c.json(`${c.var.requestId} is from ${c.var.city}`)
+})
+
+export default app
